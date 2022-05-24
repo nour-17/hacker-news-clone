93c71883d070bc6c879eab76ddba3bacc9e040f9
nour <nenohamdy32@gmail.com>
2022-05-25 00:57:44 +0200

    fixes favorites in comments

--- components/Story.js
+++ components/Story.js
@@ -15,7 +15,7 @@ export default function Story(story){
         </a>
         |
         <span class="favorite" data-story='${JSON.stringify(story)}'>
-          <img class="heart" src=${story.isFavorite?"https://cdn-icons.flaticon.com/png/128/2077/premium/2077502.png?token=exp=1653342622~hmac=564d7e7da347e72ebbd82ee70460a1fc":"https://cdn-icons-png.flaticon.com/128/1077/1077035.png"}>
+          <img class="heart" src=${story.isFavorite?"../images/heart.png":"../images/empty.png"}>
           ${story.isFavorite ? "Remove From Favorites" : "Add To Favorites"}
         </span>
       </div>
--- pages/item.js
+++ pages/item.js
@@ -1,13 +1,18 @@
 import view from "../utils/view.js";
-import Story from "../components/Story.js";
 import Comment from "../components/comment.js";
+import Favorites from "../components/favorites.js";
+import Story from "../components/Story.js";
+import checkFavorite from "../utils/checkFavorite.js"
+import store from "../store.js";
 export default async function Item(){
     let hasError = false
     let story = null;
-    let hasComments = false
+    let hasComments = false;
+    const {favorites} = store.getState()
     try {
-        story = await getStory()
+      story = await getStory()
         hasComments = story.comments.length 
+        
       }
       catch(err) {
         hasError = true
@@ -17,10 +22,19 @@ export default async function Item(){
         view.innerHTML = `<div class="error">invalid post id</div>`;
      }
     view.innerHTML = `<div>
-    ${Story(story)}
+    ${Story({...story, isFavorite:checkFavorite(favorites,story)})} 
     <hr/>
     ${`<div>${hasComments?story.comments.map(comment=>Comment(comment)).join(""):"no comments available"}</div>`}
     </div>`
+    document.querySelector(".favorite").addEventListener("click",async function(e){
+      console.log("--------------------------")
+
+      const isFavorited = checkFavorite(favorites,story)
+      store.dispatch({type: isFavorited ? "REMOVE_FAVORITE" : "ADD_FAVORITE",payload:{favorite:story}})
+      console.log(favorites)
+      console.log(store.getState())
+      Item()
+    })
 }
 async function  getStory(){
     const id = window.location.hash.split("?id=")[1];
